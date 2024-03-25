84737e24713f23f537279e9b9a7f8a61f1bbb530
USER_MA <1832389582@QQ.COM>
2024-03-25 11:03:38 +0800

    nest cli初始化

---
+++ src/app.module.ts
@@ -0,0 +1,22 @@
+import { Module } from '@nestjs/common';
+import { ProductsModule } from './products/products.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost', // 数据库ip地址
+      port: 3306, // 端口
+      username: 'root', // 登录名
+      password: 'root', // 密码
+      database: 'user', // 数据库名称
+      entities: [__dirname + '/**/*.entity{.ts,.js}'], // 扫描本项目中.entity.ts或者.entity.js的文件
+      synchronize: true, // 定义数据库表结构与实体类字段同步(这里一旦数据库少了字段就会自动加入,根据需要来使用)
+    }),
+    ProductsModule],
+  controllers: [],
+  providers: [],
+})
+export class AppModule { }
